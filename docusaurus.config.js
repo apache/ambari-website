a4e20d3afa564ed0e47cb50180cce9400dad396b
jialiang <jialiangcaimd@gmail.com>
2026-09-07 20:49:14 +0800

    AMBARI-26651: Fix 3.1 preview route checks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,6 @@ const config = {
   tagline: 'The Apache Ambari project is aimed at making Hadoop management simpler by developing software for provisioning, managing, and monitoring Apache Hadoop clusters. Ambari provides an intuitive, easy-to-use Hadoop management web UI backed by its RESTful APIs.',
   url: 'https://ambari.apache.org',
   baseUrl: '/',
-  trailingSlash: false,
   onBrokenLinks: 'warn',
   markdown: {
     hooks: {
